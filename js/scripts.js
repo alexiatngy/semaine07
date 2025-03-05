b3e757b305215772373779dd310c26f2542e7125
alexiatngy <alexiatngy@hotmail.com>
2025-03-05 16:20:59 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+const boutonsModale = document.querySelectorAll('.bouton');
+const modale = document.querySelector('.modale');
+
+
+for (let i = 0; i < boutonsModale.length; i++) {
+    boutonsModale[i].addEventListener('click', openModale);
+}
+
+
+function openModale() {
+
+const chemin = this.previousElementSibling.getAttribute("src");
+const image = document.createElement('img');
+image.setAttribute('src', chemin);
+image.classList.add('zoom');
+
+modale.appendChild(image);
+modale.classList.add('visible');
+
+
+image.addEventListener("click", closeModale);
+}
+
+function closeModale(event) {
+    modale.removeChild(event.target);
+    modale.classList.remove('visible');
+}
+
